1b1899f90373fdbef819a7f4aa5d1a83994253eb
kinoshita kazuoki <kino05040826@gmail.com>
2023-04-07 11:35:36 +0900

    generalモデルのバリデーション設定

--- app/models/general.rb
+++ app/models/general.rb
@@ -2,4 +2,7 @@ class General < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :user
+
+  validates :prefecture_id, presence: true
+  validates :address, presence: true, length: { maximum: 255 }
 end
