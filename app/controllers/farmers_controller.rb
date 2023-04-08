3c5ca940ae57ffde14109786b484c66b8b9134cc
kinoshita kazuoki <kino05040826@gmail.com>
2023-04-08 16:46:06 +0900

    farmers登録機能、詳細機能実装

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -1,6 +1,6 @@
 class FarmersController < ApplicationController
-  before_action :authenticate_user!
-  # before_action :set_general, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!
+  before_action :set_farmer, only: [:show, :edit, :update, :destroy]
   # before_action :check_general_existence, only: [:new, :create] 
 
   def index
@@ -8,12 +8,21 @@ class FarmersController < ApplicationController
   end
 
   def show
+    @user = @farmer.user
   end
 
   def new
+    @farmer = Farmer.new
   end
 
   def create
+    @farmer = Farmer.new(farmer_params)
+    @farmer.user = current_user
+    if @farmer.save
+      redirect_to @farmer, notice: "プロフィールが登録されました。"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -27,11 +36,11 @@ class FarmersController < ApplicationController
 
   private
 
-  # def general_params
-  #   params.require(:general).permit(:prefecture_id, :address)
-  # end
+  def farmer_params
+    params.require(:farmer).permit(:name, :prefecture_id, :address, :latitude, :longitude, :phone_number, :product, :website, :image, :profile)
+  end
 
-  # def set_general
-  #   @general = General.find(params[:id])
-  # end
+  def set_farmer
+    @farmer = Farmer.find(params[:id])
+  end
 end
