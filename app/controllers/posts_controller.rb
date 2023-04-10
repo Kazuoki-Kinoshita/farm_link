fa61b8f80dd9ef6d227a74d0d5c09dfd3cb80fdf
kinoshita kazuoki <kino05040826@gmail.com>
2023-04-10 17:47:40 +0900

    posts登録、詳細機能実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_unless_farmer, only: [:index, :new, :create, :edit, :update, :destroy]
+ 
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.farmer.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿が保存されました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, images: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def redirect_unless_farmer
+    if current_user.nil? || current_user.general?
+      redirect_to root_path, alert: 'アクセス権限がありません。'
+    elsif current_user.farmer.nil?
+      redirect_to new_farmer_path, alert: 'まずはプロフィール登録をしてください。'
+    end
+  end
+end
