937703c70d5255511741bcc083be3288f3410c3f
kinoshita kazuoki <kino05040826@gmail.com>
2023-04-12 16:23:59 +0900

    messages, conversationsモデル、ルーティング設定

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2023_04_11_124433) do
+ActiveRecord::Schema.define(version: 2023_04_12_020103) do
 
   enable_extension "plpgsql"
 
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2023_04_11_124433) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id", "recipient_id"], name: "index_conversations_on_sender_id_and_recipient_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "experiences", force: :cascade do |t|
@@ -64,6 +74,17 @@ ActiveRecord::Schema.define(version: 2023_04_11_124433) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_generals_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.boolean "read"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -109,5 +130,7 @@ ActiveRecord::Schema.define(version: 2023_04_11_124433) do
   add_foreign_key "experiences", "farmers"
   add_foreign_key "farmers", "users"
   add_foreign_key "generals", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "posts", "farmers"
 end
