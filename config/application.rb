b5b0cb359c7c974bba9226cb442e0b98bbf4ed01
kinoshita kazuoki <kino05040826@gmail.com>
2023-04-12 22:14:24 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module FarmLink
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
 
