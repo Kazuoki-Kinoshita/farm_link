8bf49d118ce7557c35dfa2999f2af5457029d447
kinoshita kazuoki <kino05040826@gmail.com>
2023-04-24 22:34:04 +0900

    scheduleモデルにバリデーション定義

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,16 @@
 class Schedule < ApplicationRecord
   belongs_to :experience
+
+  validates :title, presence: true, length: { maximum: 15 }
+  validate :start_time_must_be_before_end_time
+
+  scope :saved, -> { where.not(id: nil) }
+
+  private
+
+  def start_time_must_be_before_end_time
+    if start_time.present? && end_time.present? && start_time >= end_time
+      errors.add(:start_time, "は終了時間より前でなければなりません")
+    end
+  end
 end
